733f505e218f1100ddb344b40d9825546710331c
Jesse Wilson <jwilson@squareup.com>
2024-08-31 21:46:31 -0400

    Easier to debug graphs

--- kmp/shared/build.gradle.kts
+++ kmp/shared/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
   alias(libs.plugins.kotlinMultiplatform)
@@ -22,6 +23,28 @@ kotlin {
     iosTarget.binaries.framework {
       baseName = "Shared"
       isStatic = true
+    }
+  }
+
+  targets.withType<KotlinNativeTarget>().all {
+    compilations.configureEach {
+      compileTaskProvider.configure {
+        // Fail the build if we use any functions without declaring them. Otherwise, we risk getting
+        // IrLinkageError crashes at runtime if we use binary-incompatible library versions for
+        // compile and deploy.
+        compilerOptions.freeCompilerArgs.add("-Xpartial-linkage-loglevel=ERROR")
+
+        // Get comments into Shared.h
+        // https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+        compilerOptions.freeCompilerArgs.add("-Xexport-kdoc")
+
+        // This makes the Xcode memory analyzer graphs easier to understand. Be careful when using
+        // this in production; it's not as stable as the default allocator. (See KT-68769)
+        compilerOptions.freeCompilerArgs.add("-Xallocator=std")
+
+        // https://slack-chats.kotlinlang.org/t/18860817/trying-to-update-to-kotlin-2-0-0-and-when-i-build-my-ios-app
+        compilerOptions.freeCompilerArgs.add("-opt")
+      }
     }
   }
 
